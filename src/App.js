9a280a011ae62b22e7446f56e16ce9f2ffc679ec
MonikaTychy <mo.tychy@gmail.com>
2022-12-09 18:22:30 +0100

    import nanoid, setup dice to rundom nr and add funconality to roll dice and hold die

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
+import React, { useState } from 'react'
 import './App.css';
 import Die from './Die'
+import { nanoid } from 'nanoid'
 
 export default function App() {
+
+  const [dice, setDice] = useState(newDice())
+
+  function newDice() {
+    const newDice = new Array(10)
+    .fill()
+   .map(e => e = {
+       value: Math.ceil(Math.random() * 6),
+       isHeld: false,
+       id: nanoid()
+   })
+
+    return newDice
+}
+
+function rollDice() {
+  setDice(newDice())
+}
+
+function holdDie(id) {
+  setDice(prevDice => prevDice.map(die => {
+      return die.id === id ? {...die, isHeld: !die.isHeld}
+      : die
+    })
+  )
+}
+
+const diceElements = dice.map( die => <Die
+  value = {die.value}
+  key = {die.id}
+  id = {die.id}
+  isHeld = {die.isHeld}
+  holdDie = {() => holdDie(die.id)}
+  />)
+
   return (
     <div className="App">
       <main>
         <h1 className='title'>Tenzies</h1>
         <p className='instruction'>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
         <div className='dice-container'>
-          <Die value='1' />
-          <Die value='2' />
-          <Die value='3' />
-          <Die value='4' />
-          <Die value='5' />
-          <Die value='6' />
-          <Die value='1' />
-          <Die value='2' />
-          <Die value='3' />
-          <Die value='4' />
+         {diceElements}
         </div>
-        <button>Roll</button>
+        <button onClick={rollDice}>Roll</button>
       </main>
     </div>
   );
--- src/Die.js
+++ src/Die.js
@@ -2,8 +2,13 @@ import React from 'react'
 import './Die.css'
 
 export default function Die(props) {
+
+    const styles = {
+       backgroundColor: props.isHeld ? "#ffc0cb" : "#fff"
+    }
+
     return (
-        <div className='die-face'>
+        <div className='die-face' onClick={props.holdDie} style={styles}>
             <span className=''>{props.value}</span>
         </div>
     )
