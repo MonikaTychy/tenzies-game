255ffd02261c5f71f1f03d8f19fc3064441a6a58
MonikaTychy <mo.tychy@gmail.com>
2022-12-09 20:03:52 +0100

    added external component, functionality to end game and set new game

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css';
 import Die from './Die'
 import { nanoid } from 'nanoid'
+import Confetti from 'react-confetti'
 
 export default function App() {
 
   const [dice, setDice] = useState(newDice())
+  const [gameOver, setGameOver] = useState(false)
+
+  useEffect(() => {
+     const allHeld = dice.every(die => die.isHeld)
+     const firstValue = dice[0].value
+     const allTheSameValue = dice.every(die => die.value === firstValue)
+    //all the same values and all held setGameOver(true)
+    if (allHeld && allTheSameValue) {
+      setGameOver(true)
+    }
+  }, [dice])
+
+  function createNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid()
+  }
+  }
 
   function newDice() {
     const newDice = new Array(10)
     .fill()
-   .map(e => e = {
-       value: Math.ceil(Math.random() * 6),
-       isHeld: false,
-       id: nanoid()
-   })
+   .map(e => e = createNewDie())
 
     return newDice
 }
 
 function rollDice() {
-  setDice(newDice())
+  if (gameOver) {
+    setDice(newDice)
+    setGameOver(false)
+  } else {
+    setDice(prevDice => prevDice.map(die => {
+      return die.isHeld ? die : createNewDie()
+    }))
+  }
 }
 
 function holdDie(id) {
@@ -41,13 +64,14 @@ const diceElements = dice.map( die => <Die
 
   return (
     <div className="App">
+      {gameOver && <Confetti />}
       <main>
         <h1 className='title'>Tenzies</h1>
         <p className='instruction'>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
         <div className='dice-container'>
          {diceElements}
         </div>
-        <button onClick={rollDice}>Roll</button>
+        <button onClick={rollDice}>{gameOver ? "New Game" : "Roll"}</button>
       </main>
     </div>
   );
